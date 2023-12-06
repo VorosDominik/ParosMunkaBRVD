dcb427b287fe9a812462873173bc836393dcd2b6
ASDleves <ricsi20020302@gmail.com>
2023-12-06 13:15:59 +0100

    konyvator feladatresz

--- src/main/Konyvtar.java
+++ src/main/Konyvtar.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-/**
- *
- * @author BellaRichárd(SZF_202
- */
 public class Konyvtar {
-    
+
+    private AbstractTermek[] termekek;
+    private int termekDb;
+
+    public Konyvtar(int db) {
+        termekDb = 0;
+        termekek = new AbstractTermek[db];
+    }
+
+    public void felvesz(AbstractTermek termek) {
+        if (termekDb < termekek.length) {
+            termekek[termekDb++] = termek;
+        } else {
+            System.out.println("A Könyvtárban nincs hely!");
+        }
+    }
+
+    public AbstractTermek[] getTermekek() {
+        AbstractTermek[] jelenlegitermek = new AbstractTermek[this.termekek.length];
+        for (int i = 0; i < termekDb; i++) {
+            AbstractTermek aktualisTermek = this.termekek[i];
+            if (aktualisTermek != null) {
+                jelenlegitermek[i] = aktualisTermek;
+            }
+        }
+        return jelenlegitermek;
+
+    }
+
+    public void termekek() {
+        for (AbstractTermek termek : getTermekek()) {
+            if (termek != null) {
+                System.out.println(termek);
+            }
+        }
+    }
+
+    public void run() {
+        
+    }
 }
